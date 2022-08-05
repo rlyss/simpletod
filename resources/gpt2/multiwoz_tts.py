3c5fd9c0187a87a7f59891fa0108b29cf2a2ada2
rlyss <rwlau@andrew.cmu.edu>
2022-08-05 02:04:58 -0400

    Synthetic data generation multiprocessing for TTS and Hubert features

--- resources/gpt2/multiwoz_tts.py
+++ resources/gpt2/multiwoz_tts.py
@@ -25,62 +25,79 @@ text2speech = Text2Speech.from_pretrained(
     noise_scale_dur=0.333,
 )
 
-import pathlib
 import re
 import os
-import time
 import torch
 from IPython.display import display, Audio
-from pathlib import Path
 import json
 
-import string
-from torch import true_divide
+import torch.multiprocessing as mp
 from transformers import Wav2Vec2Processor, HubertModel, HubertConfig
-from datasets import load_dataset
 import soundfile as sf
 import joblib
 import json
 import argparse
+import time
 
+# Filepaths
+dir_path = os.path.abspath(__file__)
+out_path = os.path.dirname(dir_path)
+
+# Initialise parser and multiprocessing
 parser = argparse.ArgumentParser()
 parser.add_argument('--splits', type=str, nargs='*', default=['test', 'train', 'val'])
+parser.add_argument('--batches', type=int, default=1)
+mp.set_start_method('spawn', force=True)
+
+# Hubert configs and model
+processor = Wav2Vec2Processor.from_pretrained("facebook/hubert-large-ls960-ft")
+config = HubertConfig.from_pretrained("facebook/hubert-base-ls960")
+model = HubertModel.from_pretrained("facebook/hubert-base-ls960")
+
+# Hubert km model
+km_100_path = os.path.join(out_path, "hubert_km/km_100.bin")
+km_model = joblib.load(open(km_100_path, "rb"))
+km_model.verbose = True
+
+# Convert input string tts_utt to list of hubert features
+def get_audio(id, tts_utt):
+  audio = text2speech(tts_utt)["wav"]
+  input_values = processor(audio, return_tensors="pt").input_values
+
+  # Extract hubert features
+  hidden_states = model(input_values).last_hidden_state
+  discrete_units = km_model.predict(hidden_states.detach().numpy()[0])
+  return id, discrete_units.tolist()
 
 def main():
-  # Filepaths
-  dir_path = os.path.abspath(__file__)
-  out_path = os.path.dirname(dir_path)
+  args = parser.parse_args()
 
-  # Hubert configs and model
-  processor = Wav2Vec2Processor.from_pretrained("facebook/hubert-large-ls960-ft")
-  config = HubertConfig.from_pretrained("facebook/hubert-base-ls960")
-  model = HubertModel.from_pretrained("facebook/hubert-base-ls960")
-
-  # Hubert km model
-  km_100_path = os.path.join(out_path, "hubert_km/km_100.bin")
-  km_model = joblib.load(open(km_100_path, "rb"))
-  km_model.verbose = True
-
-  # Iterate each line in *.history_belief
   splits = args.splits
   with torch.no_grad(): 
     for split in splits:
-      Path(f'{out_path}/{split}_audio').mkdir(parents=True, exist_ok=True)
       # Counter for utt id
       counter = 0
-      # Dictionary for utt ids to hubert features
+      # Maps utt id to list of hubert features
       id_to_hubert = {}
+      # Maps utt to utt id
       utt_to_id = {}
+
+      # Multiprocessing batch
+      # Stores up to args.batches utts into the batch queue before processing
+      batch_queue = []
+      batch_size = args.batches
+      pool = mp.Pool(processes=3)
 
       # Open *.history_belief file and save results to *.history_belief_hub file
       with open(f'{out_path}/{split}.history_belief', 'r') as f, \
       open(f'{out_path}/{split}.history_belief_hub', 'w+') as out:
         for line in f:
-          # Split by tokens to get dialog turns
+          # Split by separator tokens (e.g. <|user|>) to get list of dialog turn utts
           utts = re.split(r'<\|.*?\|>', line)
           utts = [x for x in utts if (not x.isspace()) and x != ' \n' and x != '\n' and x != '']
 
-          # Join word units for tts
+          # Join together word units so utt can be passed through tts
+          # (e.g. typical -ly gets changed to typically)
           def to_tts(utt):
             for_tts = re.sub(r'\s+\-','', utt)
             for_tts = for_tts.replace(' s ', 's ')
@@ -88,33 +105,42 @@ def main():
           tts_utts = [to_tts(x) for x in utts]
           
           # Go through each dialog turn in the line
+          # utt - original utterance, tts_utt - processed utterance to pass to tts
           for utt, tts_utt in zip(utts, tts_utts):
-            audio = ''
-
-            # If not already generated, get tts audio and get hubert units
+            # If discrete units for utt not already generated, get hubert units
             if utt not in utt_to_id:
               # Increment and generate id for dialog turn
               counter += 1
               audio_id = f'{split}_{str(counter)}'
               utt_to_id[utt] = audio_id
 
-              # Get audio
-              audio = text2speech(tts_utt)["wav"]
-              input_values = processor(audio, return_tensors="pt").input_values
+              # Add to batch queue for processing
+              batch_queue.append((audio_id, tts_utt))
+              # If the queue hits the batch size, process all at once
+              if (len(batch_queue) == batch_size):
+                # Process queue
+                results = pool.starmap(get_audio, batch_queue)
+                print(dict(results))
+                # Merge dict with id_to_hubert
+                id_to_hubert = id_to_hubert | dict(results)
+                # Reset queue
+                batch_queue = []
 
-              # Extract hubert features
-              hidden_states = model(input_values).last_hidden_state
-              discrete_units = km_model.predict(hidden_states.detach().numpy()[0])
-              id_to_hubert[audio_id] = discrete_units.tolist()
-
-              print(discrete_units)
-
+            # Get audio id from utt_to_id
             audio_id = utt_to_id[utt]
+            # Add audio id in front of original utt
             line = line.replace(utt, f' [{audio_id}]{utt}')
           
+          # Write line with audio ids to file
           print(line)
           out.write(line)
-      
+
+      # Process remaining utts in the queue
+      results = pool.starmap(get_audio, batch_queue)
+      print(dict(results))
+      id_to_hubert = id_to_hubert | dict(results)
+      batch_queue = []
+      # Save id_to_hub dictionary to file
       with open(f'{out_path}/{split}.id_to_hub', 'w+') as link:
         link.write(json.dumps(id_to_hubert))
       
